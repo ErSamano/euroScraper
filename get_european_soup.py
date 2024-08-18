800088b91da63fc921b96831eb43dbdbe475af82
Ernesto Esparaquia <esparaquia@outlook.com>
2024-08-18 17:39:46 +0200

    Updating code with new features

--- get_european_soup.py
+++ get_european_soup.py
@@ -3,9 +3,58 @@ import pandas as pd
 import requests
 from datetime import date
 
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_data(product_type, product_url):
-    response = requests.get(product_url).content
+    '''
+            When the content page is dynamic, we need to anable a way to load
+            the hiden html site content. In order to achieve that we are going
+            to use Selenium Lib
+            to load dynamic elements.
+    '''
+
+    # s = Service(ChromeDriverManager().install())
+    # driver = webdriver.Chrome(service=s)
+    driver = webdriver.Firefox()  # Use Firefox, Chrome, etc. depending on your preference
+    driver.set_window_size(2304, 1440)
+    driver.get(product_url)
+
+    # Setting up a waiting time before scroll down
+    SCROLL_PAUSE_TIME = 6.0  # Increase the pause time if necessary
+    EXTRA_WAIT_TIME = 5.0  # Additional waiting time after scrolling
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page using WebDriverWait
+        WebDriverWait(driver, SCROLL_PAUSE_TIME).until(EC.presence_of_element_located((By.TAG_NAME, "footer")))
+
+        # Additional wait
+        time.sleep(EXTRA_WAIT_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        # Break the loop when either new_height == last_height (page end) or the specific HTML block is found
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    response = driver.execute_script("return document.documentElement.outerHTML")
+    driver.quit()
+
+    ## start the scraper
+
     soup = BeautifulSoup(response, 'html.parser')
 
     products_type = []
@@ -15,24 +64,30 @@ def get_data(product_type, product_url):
     products_old_price = []
     products_date = []
 
-    # Get the product name and the link
-    for contentProduct in soup.findAll(attrs={'class': 'product name product-item-name'}):
-        products_name.append(contentProduct.find('a').text)
-        products_type.append(product_type)
-        products_date.append(date.today())
-        for links in contentProduct.find_all('a'):
-            products_link.append(links.get('href'))
+    # Target the specific div by its class
+    target_div = soup.find('div', class_='products wrapper grid products-grid')
 
-    # Get the products price
-    for contentPrice in soup.findAll(attrs={'class': 'price-box price-final_price'}):
+    # If the div is found, proceed with the parsing
+    if target_div:
 
-        if contentPrice.find(class_='old-price'):
-            products_old_price.append(contentPrice.find(class_='old-price').text)
-            products_price.append(contentPrice.find(class_='special-price').text)
+        # Get the product name and the link
+        for contentProduct in target_div.findAll(attrs={'class': 'product name product-item-name'}):
+            products_name.append(contentProduct.find('a').text)
+            products_type.append(product_type)
+            products_date.append(date.today())
+            for links in contentProduct.find_all('a'):
+                products_link.append(links.get('href'))
 
-        else:
-            products_old_price.append("SIN OFERTA")
-            products_price.append(contentPrice.find(class_='price').text)
+        # Get the products price
+        for contentPrice in target_div.findAll(attrs={'class': 'price-box price-final_price'}):
+
+            if contentPrice.find(class_='old-price'):
+                products_old_price.append(contentPrice.find(class_='old-price').text)
+                products_price.append(contentPrice.find(class_='special-price').text)
+
+            else:
+                products_old_price.append("SIN OFERTA")
+                products_price.append(contentPrice.find(class_='price').text)
 
     products_table = pd.DataFrame({'Tipo': products_type, 'Nombre': products_name, 'Link': products_link,
                                    'Oferta/Prec_Original': products_old_price, 'Precio': products_price,
--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from get_european_soup import get_data
 
 products = {
-    'Tequila': 'https://www.laeuropea.com.mx/licores-y-destilados/tequilas.html?product_list_limit=144',
-    'Ron': 'https://www.laeuropea.com.mx/licores-y-destilados/ron.html?product_list_limit=144',
-    'Whisky': 'https://www.laeuropea.com.mx/licores-y-destilados/whisky.html?product_list_limit=144',
-    'Vodka': 'https://www.laeuropea.com.mx/licores-y-destilados/vodka.html?product_list_limit=144',
-    'Ginebra': 'https://www.laeuropea.com.mx/licores-y-destilados/ginebra.html?product_list_limit=144',
-    'Mezcal': 'https://www.laeuropea.com.mx/licores-y-destilados/mezcales.html?product_list_limit=144',
-    'Cremas/Licores': 'https://www.laeuropea.com.mx/licores-y-destilados/cremas-y-licores.html?product_list_limit=144',
-    'Cognag': 'https://www.laeuropea.com.mx/licores-y-destilados/cognac.html?product_list_limit=144',
-    'Brandy': 'https://www.laeuropea.com.mx/licores-y-destilados/brandy.html?product_list_limit=144',
-    'Sake': 'https://www.laeuropea.com.mx/licores-y-destilados/sake.html?product_list_limit=144',
-    'Otro': 'https://www.laeuropea.com.mx/licores-y-destilados/otros.html?product_list_limit=144',
-    'Vinos': 'https://www.laeuropea.com.mx/vinos/tipos/todos.html?product_list_limit=144',
-    'Vinos': 'https://www.laeuropea.com.mx/vinos/tipos/todos.html?p=2&product_list_limit=144',
-    'Vinos': 'https://www.laeuropea.com.mx/vinos/tipos/todos.html?p=3&product_list_limit=144',
-    'Vinos': 'https://www.laeuropea.com.mx/vinos/tipos/todos.html?p=4&product_list_limit=144'
+    'Vinos': 'https://www.laeuropea.com.mx/vinos/tipos/todos.html',
+    'Tequila':'https://www.laeuropea.com.mx/licores-y-destilados/tequilas.html',
+    'Ron': 'https://www.laeuropea.com.mx/licores-y-destilados/ron.html',
+    'Whisky': 'https://www.laeuropea.com.mx/licores-y-destilados/whisky.html',
+    'Vodka': 'https://www.laeuropea.com.mx/licores-y-destilados/vodka.html',
+    'Ginebra': 'https://www.laeuropea.com.mx/licores-y-destilados/ginebra.html',
+    'Mezcal': 'https://www.laeuropea.com.mx/licores-y-destilados/mezcales.html',
+    'Cremas/Licores': 'https://www.laeuropea.com.mx/licores-y-destilados/cremas-y-licores.html',
+    'Cognag': 'https://www.laeuropea.com.mx/licores-y-destilados/cognac.html',
+    'Brandy': 'https://www.laeuropea.com.mx/licores-y-destilados/brandy.html',
+    'Sake': 'https://www.laeuropea.com.mx/licores-y-destilados/sake.html',
+    'Otro': 'https://www.laeuropea.com.mx/licores-y-destilados/otros.html',
 }
 
 count = 0
-for type, url in products.items():
+for ptype, url in products.items():
     if count == 0:
-        get_data(type, url).to_csv('europea_precios.csv', index=False)
+        get_data(ptype, url).to_csv('europea_precios.csv', index=False)
     else:
-        get_data(type, url).to_csv('europea_precios.csv', mode='a', index=False, header=False)
+        get_data(ptype, url).to_csv('europea_precios.csv', mode='a', index=False, header=False)
     count += 1
